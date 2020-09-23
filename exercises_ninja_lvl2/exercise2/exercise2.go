11abe9babf14380ee73060421a1f7ca4c35c09b5
Melton <m3ltd0wn@localhost.localdomain>
2020-09-23 11:23:51 -0700

    exercise 2 & 3

---
+++ exercises_ninja_lvl2/exercise2/exercise2.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	a := (142 == 15)
+	b := (142 <= 15)
+	c := (142 >= 15)
+	d := (142 != 15)
+	e := (142 < 15)
+	f := (142 > 15)
+	fmt.Println(a, b, c, d, e, f)
+
+}
---
+++ exercises_ninja_lvl2/exercise3/exercise3.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+const (
+	a     = 142
+	b int = 15
+)
+
+func main() {
+	fmt.Println(a, b)
+}
